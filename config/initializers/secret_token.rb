7882aa43d80bf17c61e9079b4f322b5b7a960212
git-username <git-email-address>
2012-08-15 12:58:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geomap::Application.config.secret_token = 'c271b52bdfaf632456f3d39f3d8feaf63fbd15b3f536b4c5c54c0e679058f600985eff757f0f94628ef4dec18b8630268fb2a907346394076d77cb2a388fd333'
